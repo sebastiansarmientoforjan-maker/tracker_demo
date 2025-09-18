fe9e1cbe6db2d2996f93f7a8f584e24f65cb646c
sebastiansarmientoforjan-maker <sebastian.sarmiento.forjan@gmail.com>
2025-09-18 00:56:35 -0300

    Update EditScheduleModal.tsx

--- src/components/EditScheduleModal.tsx
+++ src/components/EditScheduleModal.tsx
@@ -2,9 +2,6 @@
 
 import React from "react";
 
-/**
- * Placeholder modal for editing scheduled activities. Hidden by default.
- */
 const EditScheduleModal: React.FC = () => {
   return <div className="hidden" />;
 };
