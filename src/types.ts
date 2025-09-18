96d6ebb01e503341f7524fb51ea1999ff2c016f7
sebastiansarmientoforjan-maker <sebastian.sarmiento.forjan@gmail.com>
2025-09-18 00:56:47 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,3 @@
-/**
- * Represents an unscheduled activity. These items can be dragged from the
- * unscheduled list onto the calendar. They do not yet have a class or date.
- */
 export interface Activity {
   id: string;
   title: string;
@@ -9,10 +5,6 @@ export interface Activity {
   classType: string;
 }
 
-/**
- * Represents an activity scheduled on the calendar. Extends Activity by
- * specifying the class and date to which it has been assigned.
- */
 export interface ScheduledActivity extends Activity {
   class: string;
   date: string;
