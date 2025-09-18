af79a8a69a20347430f22edf5f52a1e792446afd
sebastiansarmientoforjan-maker <sebastian.sarmiento.forjan@gmail.com>
2025-09-18 00:56:08 -0300

    Update HolidayModal.tsx

--- src/components/HolidayModal.tsx
+++ src/components/HolidayModal.tsx
@@ -2,9 +2,6 @@
 
 import React from "react";
 
-/**
- * Placeholder modal for selecting holidays. Currently hidden; implement as needed.
- */
 const HolidayModal: React.FC = () => {
   return <div className="hidden" />;
 };
